f08425df9580f3fe5911978ebce0224bdd4d97f8
leestephens12 <77406283+leestephens12@users.noreply.github.com>
2022-03-23 12:07:04 -0400

    Login and register with firebase now functional

--- app/src/main/java/com/stephensapps/boxoffice_ls/LoginActivity.kt
+++ app/src/main/java/com/stephensapps/boxoffice_ls/LoginActivity.kt
@@ -1,11 +1,107 @@
 package com.stephensapps.boxoffice_ls
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.login_activity)
+
+        //get current instance of firebase
+        auth = FirebaseAuth.getInstance()
+        //Creating vals for fields in the view
+        val submit = findViewById<Button>(R.id.submitButton)
+        val register = findViewById<Button>(R.id.registerButton)
+        val email = findViewById<EditText>(R.id.emailEditText)
+        val password = findViewById<EditText>(R.id.passwordEditText)
+        val errorText = findViewById<TextView>(R.id.errorTextView)
+        val logo = findViewById<ImageView>(R.id.logoImage)
+
+        /**
+         *Animations
+         **/
+        submit.alpha = 0f
+        submit.translationY = 75f
+        submit.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        register.alpha = 0f
+        register.translationY = 75f
+        register.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        email.alpha = 0f
+        email.translationY = 75f
+        email.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        password.alpha = 0f
+        password.translationY = 75f
+        password.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        logo.alpha = 0f
+        logo.translationY = 75f
+        logo.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        submit.setOnClickListener {
+
+            //setting default colors to text
+            email.setHintTextColor(Color.WHITE)
+            password.setHintTextColor(Color.WHITE)
+            email.setTextColor(Color.WHITE)
+            password.setTextColor(Color.WHITE)
+            errorText.setTextColor(Color.RED)
+
+            if(email.text.toString().isEmpty()) {
+                errorText.text = "Field must be complete"
+                email.setHintTextColor(Color.RED)
+            }
+            else if(password.text.toString().isEmpty()) {
+                errorText.text = "Field must be complete"
+                password.setHintTextColor(Color.RED)
+            }
+            else {
+                //using the firebase auth to sign in with email and password input by user
+                auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnCompleteListener(this) { task: Task<AuthResult> ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(baseContext, "Login successful", Toast.LENGTH_SHORT)
+                                .show()
+
+                            //after login is successful redirect to the main page
+                            intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+
+                        } else {
+                            Toast.makeText(baseContext, "Login unsuccessful", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }.addOnFailureListener(this) { exception ->
+                        errorText.text = exception.localizedMessage.toString()
+
+                        //if the error text has password in it password will show in red or else email will show in red
+                        if (errorText.text.contains("password")) {
+                            password.setTextColor(Color.RED)
+                        } else {
+                            email.setTextColor(Color.RED)
+                        }
+                    }
+
+            }
+        }
+
+        //if users don't have an account they can register here
+        register.setOnClickListener {
+            intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/stephensapps/boxoffice_ls/RegisterActivity.kt
+++ app/src/main/java/com/stephensapps/boxoffice_ls/RegisterActivity.kt
@@ -1,11 +1,157 @@
 package com.stephensapps.boxoffice_ls
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.SignInMethodQueryResult
 
 class RegisterActivity : AppCompatActivity() {
+    //setting up val for the firebase authentication
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_activity);
+        setContentView(R.layout.register_activity)
+
+        //Initializing variable to get the current instance to create the new account
+        auth = FirebaseAuth.getInstance()
+        //Initializing vals to get the user inputted text
+        val email = findViewById<EditText>(R.id.emailEditText)
+        val password = findViewById<EditText>(R.id.passwordEditText)
+        val confirmPassword = findViewById<EditText>(R.id.confirmPasswordEditText)
+        val errorText = findViewById<TextView>(R.id.errorTextView)
+        val firstName = findViewById<EditText>(R.id.firstNameEditText)
+        val lastName = findViewById<EditText>(R.id.lastNameEditText)
+        val submit = findViewById<Button>(R.id.submitButton)
+        val signIn = findViewById<Button>(R.id.signInButton)
+        val logo = findViewById<ImageView>(R.id.logoImage)
+
+        /**
+         * Animations
+         */
+
+        email.alpha = 0f
+        email.translationY = 75f
+        email.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        password.alpha = 0f
+        password.translationY = 75f
+        password.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        confirmPassword.alpha = 0f
+        confirmPassword.translationY = 75f
+        confirmPassword.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        firstName.alpha = 0f
+        firstName.translationY = 75f
+        firstName.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        lastName.alpha = 0f
+        lastName.translationY = 75f
+        lastName.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        submit.alpha = 0f
+        submit.translationY = 75f
+        submit.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+        signIn.alpha = 0f
+        signIn.translationY = 75f
+        signIn.animate().alpha(1f).translationYBy(-75f).setDuration(1500)
+
+
+        //when the submit button is clicked it will pass the email and password to firebase to create a new user account
+        submit.setOnClickListener {
+
+            //Resetting Text colors to their original values
+            email.setTextColor(Color.WHITE)
+            password.setTextColor(Color.WHITE)
+            confirmPassword.setTextColor(Color.WHITE)
+            firstName.setTextColor(Color.WHITE)
+            lastName.setTextColor(Color.WHITE)
+            errorText.setTextColor(Color.RED)
+
+            //Setting hints back to original color
+            email.setHintTextColor(Color.WHITE)
+            password.setHintTextColor(Color.WHITE)
+            confirmPassword.setHintTextColor(Color.WHITE)
+            firstName.setHintTextColor(Color.WHITE)
+            lastName.setHintTextColor(Color.WHITE)
+
+            //Error checking to make it more user friendly to show which fields are not filled out
+            if(email.text.toString().isEmpty()) {
+                errorText.text = "Field must be complete"
+                email.setHintTextColor(Color.RED)
+            }
+            else if(password.text.toString().isEmpty()) {
+                errorText.text = "Field must be complete"
+                password.setHintTextColor(Color.RED)
+            }
+            else if(firstName.text.toString().isEmpty()) {
+                errorText.text = "Field must be complete"
+                firstName.setHintTextColor(Color.RED)
+            }
+            else if(lastName.text.toString().isEmpty()) {
+                errorText.text = "Field must be complete"
+                lastName.setHintTextColor(Color.RED)
+            }
+            else if(confirmPassword.text.toString().isEmpty()) {
+                errorText.text = "Field must be complete"
+                confirmPassword.setHintTextColor(Color.RED)
+            }
+            else if(password.text.toString() == confirmPassword.text.toString()) {
+                //Checks all query results to see if email is already being used
+                auth.fetchSignInMethodsForEmail(email.text.toString())
+                    .addOnCompleteListener(this) { task: Task<SignInMethodQueryResult> ->
+                        if(task.result?.signInMethods.isNullOrEmpty()) {
+                            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                                .addOnCompleteListener(this) { task: Task<AuthResult> ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(baseContext, "Sign-up successful", Toast.LENGTH_SHORT)
+                                            .show()
+                                        //Bring you the the main activity once the registration is successful
+                                        intent = Intent(this, MainActivity::class.java)
+                                        startActivity(intent)
+                                        finish()
+                                    } else {
+                                        Toast.makeText(baseContext, "Sign-up unsuccessful", Toast.LENGTH_SHORT)
+                                            .show()
+                                    }
+                                }.addOnFailureListener(this) { exception ->
+
+                                    //If the registration fails it will output to exception directly from firebase and display it for the user
+                                    errorText.text = exception.localizedMessage.toString()
+
+                                    if(errorText.text.toString() == "The email address is badly formatted.") {
+                                        email.setTextColor(Color.RED)
+                                    }
+                                }
+                        }
+                        else {
+                            //if the email is already being used it will throw an error to the user
+                            email.setTextColor(Color.RED)
+                            errorText.text = "This Email is Already in Use"
+                        }
+                    }
+            }
+            else {
+                //if the confirm password field does not match with the password field it will throw an error and display it
+                errorText.text = "Passwords do Not Match"
+                password.setTextColor(Color.RED)
+                confirmPassword.setTextColor(Color.RED)
+            }
+        }
+
+        //if user already has an account they can sign up here
+        signIn.setOnClickListener {
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
